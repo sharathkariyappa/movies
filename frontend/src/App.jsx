ffb50d4b7b2a352a129ac2f8afcf3136eac7473b
Krupa <sharathkariyappa98@gmail.com>
2025-01-03 10:06:28 +0530

    Added global provider to the app page and fixed the favorites section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,19 @@ import {Routes, Route} from "react-router-dom"
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Favorites from "./pages/Favorites"
+import { MovieProvider } from './contexts/MovieContext'
 
 function App() {
-  const movieNumber =2
-
   return (
-    <main className='main-content'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/favorites' element={<Favorites />} />
-      </Routes>
-    </main>
+    <MovieProvider>
+      <Navbar />   
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
 
   )
 }
--- frontend/src/components/Moviecard.jsx
+++ frontend/src/components/Moviecard.jsx
@@ -1,16 +1,23 @@
 import "../css/Moviecard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
 function Moviecard({movie}){
+    const {addtoFavorites,
+        removeFromFavorites,
+        isFavorites} = useMovieContext()
+    const favorite = isFavorites(movie.id)
 
-    function onliked(){
-        alert("clikced")
+    function onFavoriteClick(){
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addtoFavorites(movie)
     }
 
     return (
         <div className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                <button className="movie-overlay" onClick={onliked}>
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
                     🤍
                 </button>
             </div>
