62a4171d74897bbd81798d78432ee8c8169af6e3
Krupa <sharathkariyappa98@gmail.com>
2025-01-03 09:28:27 +0530

    Added useEffect function to control the render

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -4,6 +4,17 @@ const MovieContext = createContext();
 
 export const useMovieContext = ()=> useContext(MovieContext);
 
-export const MovieProvider = ()=>{
-    
+export const MovieProvider = ({children})=>{
+
+    const [favorites, setFavorites] = useState([])
+
+    useEffect(()=>{
+        const storedFavs = localStorage.getItem("favorites")
+        if(storedFavs) setFavorites(JSON.parse(storedFavs))
+    },[])
+
+    return <MovieContext.Provider>
+        {children}
+    </MovieContext.Provider>
+
 }
