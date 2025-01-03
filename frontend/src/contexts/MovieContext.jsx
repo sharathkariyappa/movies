3bb0df73c99cec621e103e1dcd52cbce80cbb864
Krupa <sharathkariyappa98@gmail.com>
2025-01-03 09:34:27 +0530

    Added function AddtoFavorites

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -13,6 +13,14 @@ export const MovieProvider = ({children})=>{
         if(storedFavs) setFavorites(JSON.parse(storedFavs))
     },[])
 
+    useEffect(()=>{
+        localStorage.setItem('favorites',JSON.stringify(favorites))
+    },[favorites])
+
+    const addtoFavorites = (movie)=>{
+        setFavorites(prev => [...prev, movie])
+    }
+
     return <MovieContext.Provider>
         {children}
     </MovieContext.Provider>
